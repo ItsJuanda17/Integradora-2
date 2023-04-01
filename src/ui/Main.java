99310c1a610d58af4eae12d6a1da13d1efcf977e
ItsJuanda17 <1109115477@u.icesi.edu.co>
2023-04-01 08:09:36 -0500

    Subiendo integradora 2

---
+++ src/ui/Main.java
@@ -0,0 +1,14 @@
+package ui; 
+
+import java.util.Scanner;
+
+//Constructor de la clase 
+
+public class Main{
+
+    this.reader= new Scanner(System.in);
+}
+
+public static void main(String[] args){
+    
+}
